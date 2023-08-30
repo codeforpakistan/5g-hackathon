ef4902a8aa38771ed936492834f4f65015b012ee
Shaji Ahmed <schajee@gmai.com>
2023-08-30 15:35:41 +0500

    Added themes and domains

--- src/components/HackDomains.vue
+++ src/components/HackDomains.vue
@@ -1,9 +1,38 @@
 <template>
-  <v-container id="domains" fluid class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <h1 class="text-h2 font-weight-bold">Domains</h1>
-    </v-responsive>
-  </v-container>
+  <section id="domains" class="bg-purple-darken-4 py-md-8">
+    <v-container>
+      <v-responsive class="align-center fill-height">
+        <h2 class="text-h2 text-center font-weight-bold"><span class="text-red px-3">[</span>Hacking Domains<span class="text-red px-3">]</span></h2>
+        <v-row class="mt-8">
+          <v-col cols="12" md="6">
+            <h3 class="text-h3">Retail</h3>
+            <p>For 5G retail applications, the customer experience will be everything. Stores of tomorrow may no longer look like today’s aisles of stocked shelves. Imagine a store that’s more like a showroom—one that lets you add items to a virtual cart rather than shopping with a physical one.</p>
+            <p>Stores may also use 5G to manage inventory and stocking in real time. Consumers could even see changes like cashierless stores that simply track what you put in your cart in lieu of the traditional checkout line.</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <h3 class="text-h3">Healthcare</h3>
+            <p>5G healthcare use cases will enable doctors and patients to stay more connected than ever. Wearable devices could alert healthcare providers when a patient is experiencing symptoms—like an internal defibrillator that automatically alerts a team of ER cardiologists to be ready for an incoming patient, with a complete record of data collected by the device.</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <h3 class="text-h3">Education</h3>
+          </v-col>
+          <v-col cols="12" md="6">
+            <h3 class="text-h3">Environment</h3>
+          </v-col>
+          <v-col cols="12" md="6">
+            <h3 class="text-h3">Agriculture</h3>
+            <p>Farms of the future will use more data and fewer chemicals. Taking data from sensors located directly in fields, farmers can identify with pinpoint precision which areas need water, have a disease, or require pest management.</p>
+            <p>As wearables become less expensive and 5G makes it easier to scale networks containing large numbers of IoT devices, health monitoring for livestock may also emerge. With more accurate health data, farmers can reduce the use of antibiotics without compromising the safety of the food supply.</p>
+          </v-col>
+          <v-col cols="12" md="6">
+            <h3 class="text-h3">Industry</h3>
+            <p>Factory floors will be totally transformed by the convergence of 5G, AI, and IoT. Beyond predictive maintenance that helps control costs and minimize downtime, factories will also use 5G to control and analyze industrial processes with an unprecedented degree of precision.</p>
+            <p>With the connectivity boost provided by 5G, manufacturers can also change traditional quality assurance processes, streamlining them with sensor technology and AI.</p>
+          </v-col>
+        </v-row>
+      </v-responsive>
+    </v-container>
+  </section>
 </template>
 
 <script setup>
